4a73ac04147059c840a585453c7b6244abc5e048
Constantin Cir <constantin.cir@mocha.global>
2025-05-26 16:27:26 +0300

    Make test commit

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ def add(a, b):
 def subtract(a, b):
     return a - b
 
+def multiply(a, b):
+    return a * b
+
 if __name__ == "__main__":
     print(f"5 + 3 = {add(5, 3)}")
     print(f"5 - 3 = {subtract(5, 3)}")
+    print(f"5 * 3 = {multiply(5, 3)}")
