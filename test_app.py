998223a3f581ed4a38bf45f4b19f35c6216a4cf7
Constantin Cir <constantin.cir@mocha.global>
2025-05-26 16:52:02 +0300

    Fix test

--- test_app.py
+++ test_app.py
@@ -1,7 +1,7 @@
 # test_app.py
 
 import unittest
-from app import add, subtract
+from app import add, subtract, multiply
 
 class TestApp(unittest.TestCase):
     def test_add(self):
@@ -13,8 +13,8 @@ class TestApp(unittest.TestCase):
         self.assertEqual(subtract(0, 4), -4)
 
     def test_multiply(self):
-        self.assertEqual(subtract(5, 3), 15)
-        self.assertEqual(subtract(0, 4), 0)
+        self.assertEqual(multiply(5, 3), 15)
+        self.assertEqual(multiply(0, 4), 0)
 
 if __name__ == "__main__":
     unittest.main()
